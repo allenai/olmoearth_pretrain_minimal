28a969d0a41bdf8b3d97884e87bcbf89e0c5528b
Piper Wolters <piperwolters@gmail.com>
2026-02-03 16:07:40 -0800

    Attempt adding normalization

--- olmoearth_pretrain_minimal/__init__.py
+++ olmoearth_pretrain_minimal/__init__.py
@@ -6,11 +6,17 @@ from olmoearth_pretrain_minimal.model_loader import (
     load_model_from_path,
 )
 from olmoearth_pretrain_minimal.olmoearth_pretrain_v1 import OlmoEarthPretrain_v1
+from olmoearth_pretrain_minimal.olmoearth_pretrain_v1.data.normalize import (
+    Normalizer,
+    Strategy,
+)
 
 __all__ = [
     "OlmoEarthPretrain_v1",
     "ModelID",
     "load_model_from_id",
     "load_model_from_path",
+    "Normalizer",
+    "Strategy",
 ]
 
